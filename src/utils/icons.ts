4dc291cddabccaae27bd8e235f0efcb45fd9e912
Tanner Thomas <thetannerthomas@gmail.com>
2023-10-05 14:57:57 -0600

    add icons

---
+++ src/utils/icons.ts
@@ -0,0 +1,14 @@
+export const icons = {
+  ["twitter"]: "fa-brands fa-twitter",
+  ["facebook"]: "fa-brands fa-facebook-f",
+  ["chevron-down"]: "fa-solid fa-chevron-down",
+  ["chevron-right"]: "fa-solid fa-chevron-right",
+  ["chevron-up"]: "fa-solid fa-chevron-up",
+  ["chevron-left"]: "fa-solid fa-chevron-left",
+  ["arrow-down"]: "fa-solid fa-arrow-down",
+  ["arrow-right"]: "fa-solid fa-arrow-right",
+  ["arrow-up"]: "fa-solid fa-arrow-up",
+  ["arrow-left"]: "fa-solid fa-arrow-left",
+};
+
+export type IconName = keyof typeof icons;
